6d4be4ce07c5ed4c7927359e09029fdda7e0b5a3
Adams <98179060+edgianer@users.noreply.github.com>
2024-03-29 21:52:12 +0100

    New features added
    -Graphics changed
    -Counts number of games played
    -Deletes a letter typed in a previous character enter -> easier typing in
    -More colorful text
    -Telling you unguessed text if you lose
    -Better layout

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ from guizero import *
 from random import choice
 from wordfreq import top_n_list
 
-
-wejscie=open('dict.txt', 'r')
-words=[i.strip() for i in wejscie]
-wejscie.close()
-
-app=App("Wisielec")
-app.icon='10.jpg'
+app=App("Hangman")
+app.icon='9.PNG'
 
 up_box=Box(app, width='fill', align='top')
 left_box=Box(app,width='fill', align='left')
@@ -16,18 +11,28 @@ right_box=Box(app, width='fill', align='right')
 
 title=Text(up_box, text="Hangman", font='Comic Sans MS', size=34)
 
+middle_box=Box(left_box)
+img=Picture(middle_box, image='0.PNG', align='left', width=200, height=450)
 
-img=Picture(left_box, image='0.jpg', align='left', width=150, height=300)
+
 ###########################################
 def reset():
-    global tries, j, password, progress
+    global tries, j, password, progress, games, char_wrong
+    char_wrong=[]
     tries = 11
     j = 0
-    password = choice(list(filter(lambda x: len(x) >= 4 and "'" not in x, top_n_list('en', 1000)))).upper()
+    password = choice(list(filter(lambda x: len(x)<=9 and len(x) >= 4 and "'" not in x, top_n_list('en', 1000)))).upper()
     progress = list('_' * len(password))
-    img.image='0.jpg'
+    games+=1
+    img.image='0.PNG'
     txt_progress.value=f'{tries} tries left.'
     txt_input.value=' '.join(progress)
+    games_txt.value=f'Game number {games}.'
+    txt_char_wrong.value=''
+    txt_bx.value=''
+    password_reveal.value=''
+
+    #print(password)
 
 
 def main():
@@ -35,30 +40,40 @@ def main():
     x=txt_bx.value.upper()
     global j #bez globala, j startuje od zera
     if tries > 0:
-        # jak wczytać x??? napisać osobną funkcję!
-        #x =
         any_guessed=False
         for i in range(len(password)):
             if x == password[i]:
                 any_guessed=True
                 progress[i]= x
         txt_input.value = ' '.join(progress)
-        if progress == password:
+        if password == ''.join(progress):
             txt_progress.value = 'You win!'
+            txt_progress.text_color='green'
+            tries=-1
         if not any_guessed:
             tries -= 1
             txt_progress.value = f'{tries} tries left.'
-            j += 1
-            img.image = f"{j}.jpg"
+            if x not in char_wrong and x!='':
+                char_wrong.append(x)
+                txt_char_wrong.value=f"Wrong characters: {' '.join(char_wrong)}"
 
-    if tries == 1: #0 czy 1?
+            j += 1
+            if j<11:
+                img.image = f"{j}.PNG"
+
+    if tries == 0: #0 czy 1?
         txt_progress.value = 'You lose!'
+        password_reveal.value=f'The word unguessed: \n {password}'
+    txt_bx.value = ''
 
 ###########################################
-tries = 11
+tries = 10
 j=0
 password = choice(list(filter(lambda x: len(x)>=4 and "'" not in x,top_n_list('en', 1000)))).upper()
 progress = list('_'*len(password))
+games=1
+char_wrong=[]
+
 #print(password)
 ###########################################
 txt_input=Text(right_box, text=' '.join(progress), font='Comic Sans MS', size=20)
@@ -69,7 +84,14 @@ enter=PushButton(right_box, text='Enter', command=main) #, command=main
 reset=PushButton(right_box, text='Reset', command=reset)
 
 
+
 txt_progress=Text(right_box, text=f'{tries} tries left.', font='Comic Sans MS')
 
+
+txt_char_wrong=Text(right_box, text='', font='Comic Sans MS', color='red')
+
+games_txt=Text(right_box, text=f'Game number {games}.', font='Comic Sans MS', color='blue')
+
+password_reveal=Text(right_box, text='', font='Comic Sans MS')
 
 app.display()
